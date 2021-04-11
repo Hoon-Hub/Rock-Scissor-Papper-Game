1a7cbffb43830635fdb26e5c68cb9488ee1ad018
Hoon-Hub <gwi01304@naver.com>
2021-04-11 22:32:23 +0900

    javascript : arrow function added

--- JavaScript/ES6/arrow-function.js
+++ JavaScript/ES6/arrow-function.js
@@ -1,10 +1,11 @@
 /// Arrow function
 // === annonimous function 일때만 사용 가능함.
 
-const sumplePrint = () => console.log('simplePrint');
+const simplePrint = () => console.log('simplePrint');
 const add = (a,b) => a+b;
 const add0 = function(a,b){return a+b;};
 
+simplePrint();
 console.log(add(1,2));
 console.log(add0(1,2));
 
